0f74de2728e877d8c9b9e2831a55763dccc5a356
Agustín <aguformoso@gmail.com>
2015-09-24 20:11:04 -0300

    Improvements in the check headers command

--- simon-web/simon_app/management/commands/checkheaders.py
+++ simon-web/simon_app/management/commands/checkheaders.py
@@ -3,28 +3,54 @@ from simon_app.models import Results, TestPoint, SpeedtestTestPoint
 
 
 class Command(BaseCommand):
-
-    def make_request(self, url, method="GET"):
+    def make_request(self, url, method="GET", count=10):
         import httplib, datetime
 
         t0 = datetime.datetime.now()
+        results = []
 
-        conn = httplib.HTTPConnection(url)
-        conn.request(method, "/")
+        for n in range(count):
+            conn = httplib.HTTPConnection(url)
+            conn.request(method, "/")
 
-        t1 = datetime.datetime.now()
-        r = conn.getresponse()
-        diff = datetime.datetime.now() - t1
+            t1 = datetime.datetime.now()
+            r = conn.getresponse()
+            diff = datetime.datetime.now() - t1
 
-        return r, diff.total_seconds() * 1000
+            results.append(diff.total_seconds() * 1000) # millis
+            conn.close()
+
+        return r, results
 
     def handle(self, *args, **options):
 
+        from sys import stdout
+        import numpy
+
         for tp in SpeedtestTestPoint.objects.all():
-            url = tp.ip_address
 
-            r, rtt_get = self.make_request(url)
-            r, rtt_head = self.make_request(url, method="HEAD")
-            diff = rtt_get - rtt_head
+            try:
+                url = tp.ip_address
 
-            print "GET: %.0f ms \t HEAD: %.0f \t Diff: %.0f" % (rtt_get, rtt_head, diff)
+                gets = []
+                heads = []
+                N = 10
+                for i in range(N):
+
+                    stdout.write("\r%.2f%%" % (100.0 * i / N))
+                    stdout.flush()
+
+                    _r, rtt_get = self.make_request(url, method="GET", count=10)
+                    _r, rtt_head = self.make_request(url, method="HEAD", count=10)
+
+                    gets += rtt_get
+                    heads += rtt_head
+
+                print "\rGET: %.0f ms \t HEAD: %.0f \t (%s)" % (numpy.average(gets), numpy.average(heads), tp.speedtest_url)
+
+            except KeyboardInterrupt:
+                raise
+            except:
+                continue
+
+            exit(0)
